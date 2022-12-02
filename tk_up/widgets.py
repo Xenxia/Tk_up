cfe96be5d674d4fc910c73c4cbe3706dbdd1585c
xenxia <xen.devops@gmail.com>
2022-12-02 21:54:19 +0100

    add tab

--- tk_up/widgets.py
+++ tk_up/widgets.py
@@ -4,6 +4,7 @@ import tkinter
 from typing import Any, Literal, Tuple
 from tkinter.constants import BOTH, BOTTOM, DISABLED, END, HORIZONTAL, LEFT, NO, NORMAL, RIGHT, VERTICAL, W, X, Y, YES
 from PIL import Image, ImageTk, ImageColor
+from copy import copy
 
 PLATFORM_SYS = platform.system()
 
@@ -982,3 +983,35 @@ class Separator_up(ttk.Separator, Widget_up):
         ttk.Separator.__init__(self, master=master, cursor=cursor, name=name, orient=orient, style=style, takefocus=takefocus)
         Widget_up.__init__(self)
 
+class Tab_up(ttk.Notebook, Widget_up):
+
+    tabs: dict[str, tuple[int, Frame_up]] = {}
+    __num: int = 0
+
+    def __init__(self, master=None, cursor=None) -> None:
+        ttk.Notebook.__init__(self, master=master, cursor=cursor)
+        Widget_up.__init__(self)
+
+    def add_tab(self, name: str) -> Frame_up:
+
+        frame = Frame_up(self)
+
+        self.tabs[name] = copy((self.__num, copy(frame)))
+
+        self.add(self.tabs[name][1], text=name)
+
+        self.__num += 1
+
+        return self.tabs[name][1]
+
+    def get_tab(self, name: str) -> Frame_up:
+        return self.tabs[name][1]
+
+    def disable_tab(self, name: str):
+        self.tab(self.tabs[name][0], state="disabled")
+
+    def enable_tab(self, name: str):
+        self.tab(self.tabs[name][0], state="normal")
+
+    def del_tab(self, name: str):
+        self.forget(self.tabs[name][0])
