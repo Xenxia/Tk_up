b1d489f39e9dd7b69558dcb45f8e8adbfbd417cc
xenxia <xen.devops@gmail.com>
2023-06-22 21:57:09 +0200

    Change system theme

--- tk_up/managerThemes.py
+++ tk_up/managerThemes.py
@@ -29,26 +29,26 @@ class ManagerThemes():
             self.list_themes.append(theme_name)
             self.themes[theme_name] = theme_path
 
-    def setTheme(self, name: str, themes: str = "light"):
+    def setTheme(self, name: str):
 
         if name in self.list_themes:
             self.master.tk.call("source", f"{self.themes[name]}")
-            self.master.tk.call("set_theme", f"{themes}")
+            # self.master.tk.call("source", f"{self.themes[name]}")
             self.use_theme = name
         else:
             raise KeyError(f"'{name}' is not valide theme")
 
         return self
 
-    def switch_Light_Dark_Theme(self):
-        if self.master.tk.call("ttk::style", "theme", "use") == f"{self.use_theme}-dark":
-            # Set light theme
-            self.master.tk.call("set_theme", "light")
-        else:
-            # Set dark theme
-            self.master.tk.call("set_theme", "dark")
+    # def switch_Light_Dark_Theme(self):
+    #     if self.master.tk.call("ttk::style", "theme", "use") == f"{self.use_theme}-dark":
+    #         # Set light theme
+    #         self.master.tk.call("set_theme", "light")
+    #     else:
+    #         # Set dark theme
+    #         self.master.tk.call("set_theme", "dark")
 
-        return self
+    #     return self
 
     def get_info_element(self, element):
         try:
